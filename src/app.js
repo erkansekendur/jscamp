4f391414d3a5e6f582e015f89b3a496fb22c6869
Erkan Sekendur <erkansekendur@gmail.com>
2021-10-21 00:44:38 +0300

    21 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("Merhaba kodlama.io")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+// const sıklıkla kullanılan
+const euroDun= 11.2
+// euroDun = 11 // Bunu yapamıyoruz //
+
+console.log(euroDun)
+
+//array = veriyi aynı değişkenlerde kullanmak için kullanıyoruz (BİRDEN FAZLA VERİLERDE)//
+//camelCasing 
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i<konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+// for döngüdür - verileri dolaşmak
+
+
+
+console.log(konutKredileri)
